d80a7f91597fb90942fa98cfd05483602fbbbda7
NomadicSasquatch <boyu.chong@gmail.com>
2024-12-09 16:04:27 +0800

    test

--- src/app/components/ControlPanel.js
+++ src/app/components/ControlPanel.js
@@ -18,7 +18,6 @@ const GridPanel = ({ grid, setGrid, resetGrid, runAlgorithm }) => {
           </button>
           <button onClick={() => runAlgorithm("A*")} className="btn btn-algo">
             Run A*
-            console.log("hello");
           </button>
         </div>
       </div>
