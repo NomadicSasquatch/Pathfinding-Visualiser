ec457f3cc23384595c5a4ba9bd166989e6e806a8
NomadicSasquatch <boyu.chong@gmail.com>
2024-12-23 23:53:45 +0800

    adjusted grid size, todolist

--- src/app/config/config.js
+++ src/app/config/config.js
@@ -1,3 +1,3 @@
 export const NODE_SIZE = 30;
-export const GRID_ROWS = 22;
-export const GRID_COLS = 55;
+export const GRID_ROWS = 55; //originally 22
+export const GRID_COLS = 110; // originally 55
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 import PathfindingVisualiser from './components/Pathfinding Visualiser/PathfindingVisualiser';
+import styles from './components/Pathfinding Visualiser/PathfindingVisualiser.module.css';
 
 export default function Home() {
   return (
     <main>
-      <PathfindingVisualiser />
+      <PathfindingVisualiser className={styles.visualizerContainer}/>
     </main>
   );
 }
