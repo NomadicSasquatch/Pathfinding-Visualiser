169dd6e11b05cc816c419b7c42d9671f9e01fa68
NomadicSasquatch <boyu.chong@gmail.com>
2025-01-08 00:32:34 +0800

    Chore: Updated slider component

--- src/app/components/Slider/Slider.js
+++ src/app/components/Slider/Slider.js
@@ -1,23 +1,23 @@
 import React, { useState } from 'react';
+import { useGlobalDelay } from '../GlobalDelayContext/GlobalDelayContext';
 
 export default function Slider() {
-    
+    const { delay, setDelay } = useGlobalDelay();
+
+    const handleChange = (e) => {
+        setDelay(Number(e.target.value));
+    };
+
     return (
-        <div>
-          <h1>Animation Speed</h1>
-          <div>
-            <label htmlFor="delay-slider">Delay (ms): {sliderValue}</label>
-            <input
-              id="delay-slider"
-              type="range"
-              min="10"
-              max="2000"
-              step="10"
-              value={sliderValue}
-              onChange={handleSliderChange}
-            />
-          </div>
-          <button onClick={asyncExampleFunction}>Run Async Function</button>
+        <div style={{ margin: '10px 0' }}>
+          <input 
+            type="range" 
+            min="10"
+            max="200"
+            value={delay} 
+            onChange={handleChange} 
+          />
+          <span style={{ marginLeft: '8px' }}>{delay} ms</span>
         </div>
       );
-}
+};
